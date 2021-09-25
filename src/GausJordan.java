c3f8fa5c92f7ed5c1b045578b09ca7d09f097a7b
voniersa <vonier1998@gmx.de>
2021-09-25 10:52:17 +0200

    Add src directory

---
+++ src/GausJordan.java
@@ -0,0 +1,332 @@
+//Erstellt von Sascha Vonier
+
+import java.util.Scanner;
+
+public class GausJordan
+{
+	public void Eingabe() //wird aufgerufen, wenn der Benutzer sein eigenes LGS lösen will
+	{
+		GausJordan gj = new GausJordan(); //Objekt der Klasse Gausjordan wird erstellt
+		double[] row1 = {0, 0, 0, 0};
+		double[] row2 = {0, 0, 0, 0};
+		double[] row3 = {0, 0, 0, 0};
+		String[] variableVector = {"", "", ""};
+		System.out.println("Koeffizientenmatrix:\n");
+		for(int i = 1; i <= 3; i++) //die vorderen drei row-Array-Einträge werden befüllt (Koeffizientenmatirx)
+		{
+			for(int j = 1; j <= 3; j++)
+			{
+				System.out.println("\nGib den Wert für x" + i + "," + j + " ein:");
+				switch(i)
+				{
+				case 1:
+					row1[j-1] = gj.readNumber();
+					break;
+				case 2:
+					row2[j-1] = gj.readNumber();
+					break;
+				case 3:
+					row3[j-1] = gj.readNumber();
+				}
+			}
+		}
+
+		//der hintere row-Array-Eintrag wird befüllt (Ergebnisvektor)
+		System.out.println("\n\nErgebnisvektor:\n\nGib den Wert für b1 ein:");
+		row1[3] = gj.readNumber();
+		System.out.println("\nGib den Wert für b2 ein:");
+		row2[3] = gj.readNumber();
+		System.out.println("\nGib den Wert für b3 ein:");
+		row3[3] = gj.readNumber();
+
+		//die Werte des Variablenvektors werden vom Benutzer benannt
+		System.out.println("\n\nVariablenvektor:");
+		for(int i = 1; i <= 3; i++)
+		{
+			System.out.print("\n\nWie soll Wert " + i + " des Variablenvektors benannt werden:\n");
+			variableVector[i-1] = gj.readString();
+			if(variableVector[i-1].equals("")) //Abfrage ob der Benutzer etwas eingegeben hat
+			{
+				System.out.print("\nGib bitte etwas ein, z.B. x1, x2, x3 oder x, y, z:\n");
+				i--;
+			}
+		}
+		System.out.println("\n\nSind ihre Eingaben korrekt? [J/n]:\n\n");
+		System.out.println(row1[0] + " " + variableVector[0] + " + " + row1[1] + " " + variableVector[1] + " + " + row1[2] + " " + variableVector[2] + " = " + row1[3] + "\n");
+		System.out.println(row2[0] + " " + variableVector[0] + " + " + row2[1] + " " + variableVector[1] + " + " + row2[2] + " " + variableVector[2] + " = " + row2[3] + "\n");
+		System.out.println(row3[0] + " " + variableVector[0] + " + " + row3[1] + " " + variableVector[1] + " + " + row3[2] + " " + variableVector[2] + " = " + row3[3] + "\n");
+		String string = gj.readString();
+
+		if(string.equals("j") || string.equals("J") || string.equals("")) //Abfrage ob der Benutzer fortfahren möchte
+		{
+			rechnen(row1, row2, row3);
+		}
+		else
+		{
+			System.out.println("\nProgramm wird beendet...");
+			System.exit(0);
+		}
+	}
+
+	/*Beschreibung: 	Ermöglicht eine Nutzereingabe, die auf Zahlen beschränkt ist, wird solange ausgeführt, bis der Benutzer eine richtige Zahl eingibt.
+	Parameter: 			-
+	Returnwert: 		Gibt die eingegebene Zahl zurück*/
+	private double readNumber()
+	{
+		Scanner scan = new Scanner( System.in );
+		boolean isDouble = false;
+		String input;
+		double d = 1.0;
+
+		while(!isDouble)
+		{
+			input = scan.nextLine();   	
+			try {
+				d = Double.parseDouble(input);
+				isDouble = true;
+			}catch(NumberFormatException e){
+				System.out.println("\n" + input + " ist keine Zahl, probiere es nochmal!");
+			}
+		}
+		return d;
+	}
+
+	/*Beschreibung: 	Ermöglicht eine Nutzereingabe.
+	Parameter: 			-
+	Returnwert: 		Gibt den eingegebenen Text des Benutzers zurück*/
+	private String readString()
+	{
+		Scanner scan = new Scanner( System.in );
+		return scan.nextLine();
+	}
+
+	/*Beschreibung: 	Wendet das Gaus Jordan Verfahren beim LGS an
+	Parameter: 			rowx (Zeile1), rowy (Zeile2), rowz (Zeile3)
+	Returnwert: 		Gibt ein Array mit 4 Einträgen zurück. Der 4. Eintrag vom Array gibt an, ob es eine, keine oder unendliche Lösungen gab.
+						Bei einer Lösung stehen in den ersten drei Arrayeinträgen die Werte */
+	public double[] rechnen(double[] rowx, double[] rowy, double[] rowz) 
+	{
+		GausJordan gj = new GausJordan(); //Objekt der Klasse Gausjordan wird erstellt
+		double[] row1 = rowx;
+		double[] row2 = rowy;
+		double[] row3 = rowz;
+		double[] ergebnis = {0, 0, 0, 0};
+		String[] variableVector = {"x", "y", "z"};
+		System.out.println("EINGABE:");
+		gj.printLine(row1, row2, row3);	
+		int i = 0;
+		while(i <= 8)
+		{
+			switch(i)
+			{
+			case 0:
+			default: break;
+			case 1:
+				if(row1[0] == 0 && row2[0] != 0) //Abfrage ob die erste Zeile mit einer Anderen getauscht werden muss
+				{
+					double[] placeholder = row1;
+					row1 = row2;
+					row2 = placeholder;
+				} else if(row1[0] == 0 && row3[0] != 0)
+				{
+					double[] placeholder = row1;
+					row1 = row3;
+					row3 = placeholder;
+				} else if(row1[0] == 0 && row2[0] == 0 && row3[0] == 0)
+				{
+					System.out.println("\nEs gibt keine Lösung.\n");
+					ergebnis[3] = 3;
+					i = 9;
+					break;
+				}
+
+				row1 = gj.getTheOne(row1, 1);
+				break;
+			case 2:
+				row2 = gj.getZeroAndColumn1IsMultiplier(row2, row1);
+				row3 = gj.getZeroAndColumn1IsMultiplier(row3, row1);
+				break;
+			case 3:
+				if(row2[1] == 0 && row3[1] != 0) //Abfrage ob die zweite Zeile mit der Dritten getauscht werden muss
+				{
+					double[] placeholder = row2;
+					row2 = row3;
+					row3 = placeholder;
+				} else if(row2[1] == 0 && row3[1] == 0)
+				{
+					System.out.println("\nEs gibt keine Lösung.\n");
+					i = 9;
+					ergebnis[3] = 3;
+					break;
+				}
+
+				row2 = gj.getTheOne(row2, 2);
+				break;
+			case 4:						
+				row3 = gj.getZeroAndColumn2IsMultiplier(row3, row2);
+
+				switch (gj.checkResult(row1, row2, row3)) //An dieser Stelle im Programm ist das Eliminationsverfahren beendet. Schaut man sich nun das Gleichungssystem an kann man genau sagen, wieviele Lösungen es gibt.
+				{
+				case 2: //wird reingesprungen, wenn das LGS unendlich viele Lösungen hat
+					gj.printLine(row1, row2, row3);
+					System.out.println("\nEs gibt unendlich viele Lösungen.\n");
+					ergebnis[3] = 2;
+					i = 9;
+					break;
+				case 3: //wird reingesprungen, wenn das LGS keine Lösung hat
+					gj.printLine(row1, row2, row3);
+					System.out.println("\nEs gibt keine Lösung.\n");
+					ergebnis[3] = 3;
+					i = 9;
+					break;
+				}
+				break;
+			case 5:
+				row3 = gj.getTheOne(row3, 3);
+				break;
+			case 6:
+				row2 = gj.getZeroAndColumn3IsMultiplier(row2, row3);
+				break;
+			case 7:
+				row1 = gj.getZeroAndColumn3IsMultiplier(row1, row3);
+				break;
+			case 8:
+				row1 = gj.getZeroAndColumn2IsMultiplier(row1, row2);
+				break;
+			}
+			i++;
+		}
+
+		if(ergebnis[3] == 0)
+		{
+			System.out.println("Ergebnis:\n" + variableVector[0] + ": " + row1[3] + ", " + variableVector[1] + ": " + row2[3] + ", " + variableVector[2] + ": " + row3[3] + "\n");
+			ergebnis[0] = row1[3];
+			ergebnis[1] = row2[3];
+			ergebnis[2] = row3[3];
+			ergebnis[3] = 1;
+		}
+		return ergebnis;
+	}
+
+	/*Beschreibung: 	Stellt die 3 Zeilen des LGS's dar
+	Parameter: 			row1 (Zeile1), row2 (Zeile2), row3 (Zeile3)
+	Returnwert: 		-*/
+	private void printLine(double[] row1, double[] row2, double[] row3)
+	{
+		System.out.println(row1[0] + " | " + row1[1] + " | " + row1[2] + " || " + row1[3]);
+		System.out.println(row2[0] + " | " + row2[1] + " | " + row2[2] + " || " + row2[3]);
+		System.out.println(row3[0] + " | " + row3[1] + " | " + row3[2] + " || " + row3[3]);
+		System.out.println();
+	}
+
+	/*Beschreibung: 	Überprüft, ob es keine, eine eindeutige oder unendlich viele Lösungen gibt. Dafür stellt die Funktion die Stufenform mithilfe des Eliminationsverfahren her.
+	Parameter: 			row1 (Zeile1), row2 (Zeile2), row3 (Zeile3)
+	Returnwert: 		Gibt eine Integerzahl zurück, die darstellt, wieviele Lösungen es gibt (siehe Legende weiter unten)*/
+	public int checkResult(double[] row1, double[] row2, double[] row3)
+	{
+		//Returnwert = 1 -> es gibt eine eindeutige Lösung (keine Zeile ist komplett null)
+		//Returnwert = 2 -> es gibt unendlich viele Lösungen (eine Zeile ist komplett null, sowie der jeweilige Ergebnisvektoreintrag)
+		//Returnwert = 3 -> es gibt keine Lösung (eine Zeile ist komplett null, der jeweilige Ergebnisvektoreintrag aber nicht)
+				
+		row1 = getTheOne(row1, 1);
+		row2 = getZeroAndColumn1IsMultiplier(row2, row1);
+		row3 = getZeroAndColumn1IsMultiplier(row3, row1);
+		row2 = getTheOne(row2, 2);
+		row3 = getZeroAndColumn2IsMultiplier(row3, row2);
+
+		if(row1[0] == 0 && row1[1] == 0 && row1[2] == 0)
+		{
+			if(row1[3] == 0)
+			{
+				return 2;
+			}else{
+				return 3;
+			}	
+		}
+		else if(row2[0] == 0 && row2[1] == 0 && row2[2] == 0)
+		{
+			if(row2[3] == 0)
+			{
+				return 2;
+			}else{
+				return 3;
+			}	
+		}
+		else if(row3[0] == 0 && row3[1] == 0 && row3[2] == 0)
+		{
+			if(row3[3] == 0)
+			{
+				return 2;
+			}else{
+				return 3;
+			}	
+		}
+		else{
+			return 1;
+		}
+	}
+
+	/*Beschreibung: 	Erzeugt die benötigte 1 in einer vorher ausgewählten Zeile in der Diagonale.
+	Parameter: 			row (Zeile in der die Eins entstehen soll), whichColumn (gibt an, in welcher Spalte die 1 enstehen soll)
+	Returnwert: 		Gibt die neue Zeile mit der entstandenden Eins zurück*/
+	public double[] getTheOne(double[] row, int whichColumn)
+	{
+		double divisor = 1;
+		switch(whichColumn)
+		{
+		case 1:
+			divisor = row[0];
+			break;
+		case 2:
+			divisor = row[1];
+			break;
+		case 3:
+			divisor = row[2];
+			break;
+		}
+		row[0] = row[0] / divisor;
+		row[1] = row[1] / divisor;
+		row[2] = row[2] / divisor;
+		row[3] = row[3] / divisor;
+		return row;
+	}
+
+	/*Beschreibung: 	Erzeugt eine Null in der ersten Spalte einer vorher ausgewählten Zeile.
+	Parameter: 			someRow (Zeile in der eine Null erzeugt werden soll), anotherRow (Zeile, mit der multipliziert wird)
+	Returnwert: 		Gibt die neue Zeile mit der entstandenden Null zurück*/
+	public double[] getZeroAndColumn1IsMultiplier(double[] someRow, double[] anotherRow)
+	{
+		someRow[1] = someRow[1] - (someRow[0] * anotherRow[1]);
+		someRow[2] = someRow[2] - (someRow[0] * anotherRow[2]);
+		someRow[3] = someRow[3] - (someRow[0] * anotherRow[3]);
+		someRow[0] = someRow[0] - (someRow[0] * anotherRow[0]);
+
+		return someRow;
+	}
+
+	/*Beschreibung: 	Erzeugt eine Null in der zweiten Spalte einer vorher ausgewählten Zeile.
+	Parameter: 			someRow (Zeile in der eine Null erzeugt werden soll), anotherRow (Zeile, mit der multipliziert wird)
+	Returnwert: 		Gibt die neue Zeile mit der entstandenden Null zurück*/
+	public double[] getZeroAndColumn2IsMultiplier(double[] someRow, double[] anotherRow)
+	{
+		someRow[0] = someRow[0] - (someRow[1] * anotherRow[0]);
+		someRow[2] = someRow[2] - (someRow[1] * anotherRow[2]);
+		someRow[3] = someRow[3] - (someRow[1] * anotherRow[3]);
+		someRow[1] = someRow[1] - (someRow[1] * anotherRow[1]);
+
+		return someRow;
+	}
+
+	/*Beschreibung: 	Erzeugt eine Null in der dritten Spalte einer vorher ausgewählten Zeile.
+	Parameter: 			someRow (Zeile in der eine Null erzeugt werden soll), anotherRow (Zeile, mit der multipliziert wird)
+	Returnwert: 		Gibt die neue Zeile mit der entstandenden Null zurück*/
+	public double[] getZeroAndColumn3IsMultiplier(double[] someRow, double[] anotherRow)
+	{
+		someRow[0] = someRow[0] - (someRow[2] * anotherRow[0]);
+		someRow[1] = someRow[1] - (someRow[2] * anotherRow[1]);
+		someRow[3] = someRow[3] - (someRow[2] * anotherRow[3]);
+		someRow[2] = someRow[2] - (someRow[2] * anotherRow[2]);
+
+		return someRow;
+	}
+}
---
+++ src/GausJordanTest.java
@@ -0,0 +1,187 @@
+//Erstellt von Thorben Böer
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+class GausJordanTest {
+
+	private GausJordan gj = new GausJordan(); //Objekt der Klasse GausJordan wird erzeugt
+
+	//hier werden zunächst die einzelen Rechen Funktionen getestet
+	@Test
+	public void getZeroAndColumn1IsMultiplierTest() 
+	{
+		double[] row2 = { 4, 3, 2, 1 }; // some
+		double[] row1 = { 1, 2, 3, 4 }; // another
+		double[] ergebnis = { 0, -5, -10, -15 }; // -3 -1 1 3
+
+		assertTrue(Arrays.equals(gj.getZeroAndColumn1IsMultiplier(row2, row1), ergebnis));
+		assertTrue(row2[0] == 0);
+	}
+
+	@Test
+	public void getZeroAndColumn2IsMultiplierTest() {
+		double[] row2 = { 0, 3, 2, 1 }; // some
+		double[] row1 = { 0, 1, 2, 3 }; // another
+		double[] ergebnis = { 0, 0, -4, -8 };
+
+		assertTrue(Arrays.equals(gj.getZeroAndColumn2IsMultiplier(row2, row1), ergebnis));
+		assertTrue(row2[1] == 0);
+	}
+
+	@Test
+	public void getZeroAndColumn3IsMultiplierTest() {
+		double[] row2 = { 0, 0, 1, 2 }; // some
+		double[] row1 = { 1, 2, 3, 4 }; // another
+		double[] ergebnis = { 1, 2, 0, -2 };
+
+		assertTrue(Arrays.equals(gj.getZeroAndColumn3IsMultiplier(row1, row2), ergebnis));
+		assertTrue(row1[2] == 0);
+	}
+
+	@Test
+	public void getTheOneTest() {		
+		int zahl1 = 1;
+		double[] row1 = {2, 4, 6, 8 };
+		double[] ergebnis1 = { 1, 2, 3, 4 };
+		assertTrue(Arrays.equals(gj.getTheOne(row1, zahl1), ergebnis1));
+		assertTrue(ergebnis1[zahl1-1] == 1);
+
+		int zahl2 = 2;
+		double[]row2 = {2, 4, 6, 8};
+		double[] ergebnis2 = {0.5, 1, 1.5, 2 };
+		assertTrue(Arrays.equals(gj.getTheOne(row2, zahl2), ergebnis2));
+		assertTrue(ergebnis2[zahl2-1] == 1);
+
+		int zahl3 = 3;
+		double[]row3 = {4, 6, 8, 10};
+		double[] ergebnis3 = { 0.5, 0.75, 1, 1.25 };
+		assertTrue(Arrays.equals(gj.getTheOne(row3, zahl3), ergebnis3));
+		assertTrue(ergebnis3[zahl3-1] == 1);
+	}
+
+	@Test
+	public void checkResultTest()
+	{
+		//Usecase: Es gibt eine eindeutige Lösung
+		double[] row1 = {1, 2, 3, 4};
+		double[] row2 = {0, 4, 3, 2};
+		double[] row3 = {0, 0, 5, 9};
+		int ergebnis = 1;
+		assertEquals(gj.checkResult(row1, row2, row3), ergebnis);
+
+		//Usecase: Es gibt unendlich viele Lösungen
+		double[] rowA = {1, 2, 3, 4};
+		double[] rowB = {0, 4, 3, 2};
+		double[] rowC = {0, 0, 0, 0};
+		int ergebnisABC = 2;
+		assertEquals(gj.checkResult(rowA, rowB, rowC), ergebnisABC);
+
+		//Usecase: Es gibt keine Lösung.
+		double[] rowX = {1, 2, 3, 4};
+		double[] rowY = {0, 4, 3, 2};
+		double[] rowZ = {0, 0, 0, 6};
+		int ergebnisXYZ = 3;
+		assertEquals(gj.checkResult(rowX, rowY, rowZ), ergebnisXYZ);
+	}
+
+	//Bei den folgenden Tests werden die 3 Usecases getestet: 1 Lösung, unendlich viele Lösungen oder keine Lösung.
+	@Test //Usecase: das LGS hat 1 Lösung
+	public void eineLoesung1()
+	{
+		double ergebnis = 1;
+		double[] rowx = {-3,  5, 6, -8};
+		double[] rowy = { 9, -9, 3, -6};
+		double[] rowz = { 9, -5, 6, -4};
+		double[] ergebnisLGS = gj.rechnen(rowx, rowy, rowz);
+
+		assertTrue(ergebnisLGS[0] == 2);
+		assertTrue(ergebnisLGS[1] == 2);
+		assertTrue(ergebnisLGS[2] == -2);
+		assertTrue(ergebnisLGS[3] == ergebnis);
+		Trennstriche();
+	}
+
+	@Test //Usecase: das LGS hat 1 Lösung (LGS wurde mit anderen Werten gefüllt)
+	public void eineLoesung2()
+	{
+		double ergebnis = 1;
+		double[] rowx = { 2, -9, -3,  0};
+		double[] rowy = {-2,  1,  3, -8};
+		double[] rowz = { 1,  7, -2,  0};
+		double[] ergebnisLGS = gj.rechnen(rowx, rowy, rowz);
+
+		assertTrue(ergebnisLGS[0] == 39);
+		assertTrue(ergebnisLGS[1] == 1);
+		assertTrue(ergebnisLGS[2] == 23);
+		assertTrue(ergebnisLGS[3] == ergebnis);
+		Trennstriche();
+	}
+
+	@Test //Usecase: das LGS hat unendlich viele Lösungen
+	public void unendlichLoesungen1()
+	{
+		double ergebnis = 2;
+		double[] rowx = {1, 2, 3, 3};
+		double[] rowy = {4, 5, 6, 6};
+		double[] rowz = {7, 8, 9, 9};
+		assertTrue((gj.rechnen(rowx, rowy, rowz))[3] == ergebnis);	
+		Trennstriche();
+	}
+
+	@Test //Usecase: das LGS hat unendlich viele Lösungen (LGS wurde mit anderen Werten gefüllt)
+	public void unendlichLoesungen2()
+	{
+		double ergebnis = 2;
+		double[] rowx = {-9, 1, -6, -8};
+		double[] rowy = { 3, 0, -2,  7};
+		double[] rowz = {-3, 0,  2, -7};
+		assertTrue((gj.rechnen(rowx, rowy, rowz))[3] == ergebnis);	
+		Trennstriche();
+	}
+
+	@Test //Usecase: das LGS hat keine Lösung
+	public void keineLoesung1()
+	{
+		double ergebnis = 3;
+		double[] rowx = {1, 2, 3, 2};
+		double[] rowy = {4, 5, 6, 1};
+		double[] rowz = {7, 8, 9, 2};
+		assertTrue((gj.rechnen(rowx, rowy, rowz))[3] == ergebnis);	
+		Trennstriche();
+	}
+
+	@Test //Usecase: das LGS hat keine Lösung (LGS wurde mit anderen Werten gefüllt)
+	public void keineLoesung2()
+	{
+		double ergebnis = 3;
+		double[] rowx = {-1, -2,  0,  0};
+		double[] rowy = { 4, -1, -2, -8};
+		double[] rowz = { 7, -4, -4, -4};
+		assertTrue((gj.rechnen(rowx, rowy, rowz))[3] == ergebnis);	
+		Trennstriche();
+	}
+
+	@Test //Lässt den Benutzer ein eigenes LGS eingeben, welches vom Programm wenn möglich gelöst wird
+	public void eigenesLGS()
+	{
+		System.out.println("Wollen Sie ein eigenes LGS lösen lassen? (J/n)");
+		Scanner scan = new Scanner( System.in );
+		String string = scan.nextLine();
+		if(string.equals("j") || string.equals("J") || string.equals(""))
+		{
+			gj.Eingabe();
+		}
+		Trennstriche();
+	}
+	
+	
+	public void Trennstriche()
+	{
+		System.out.println("------------------------------------");
+	}
+}
